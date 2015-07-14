6b564823129b6cd5511a681c13d8028397d2ea5f
Robert Plugge <robmplugge@hotmail.com>
2015-07-14 18:39:25 -0500

    Working on making click handlers for each li element

--- global.js
+++ global.js
@@ -6,10 +6,16 @@ req.addEventListener("load", function(){
   for (i=0; i < req.response.length; i++) {
     var tab = document.createElement("button")
     tab.setAttribute("class", "tab")
+    tab.classList.add(i)
     tab.setAttribute("id", "tab" + i)
     tab.innerHTML = req.response[i].general_info
     
     var container = document.getElementById("TabHeader")
+
+    tab.addEventListener("click", function(){
+      var p1 = document.createElement("p")
+      p1.innerHTML = req.response[i].github_link
+    })
     
     container.appendChild(tab)
   }
@@ -17,3 +23,5 @@ req.addEventListener("load", function(){
 
 req.responseType = "json";
 req.send();
+
+
